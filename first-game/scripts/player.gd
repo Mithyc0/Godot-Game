93bde879e4f2f64db1f247da58ca9b24c884c89f
Pior Nome <lukeloke007@gmail.com>
2026-03-02 15:39:06 -0300

    Bug arrumado
    O bug de enquanto pular não poder olhar para outra direção, agora está arrumado

--- first-game/scripts/player.gd
+++ first-game/scripts/player.gd
@@ -16,7 +16,7 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 		if is_on_floor():
 			_animated_sprite.play("run")
-			_animated_sprite.flip_h = direction < 0 
+		_animated_sprite.flip_h = direction < 0 
 	
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
@@ -26,5 +26,6 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		_animated_sprite.play("roll")
+	
 
 	move_and_slide()
