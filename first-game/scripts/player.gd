e11f577f59f44fb92d884d05d679e11a876a107a
Ismael Paez do Carmo <mithyc.paez@gmail.com>
2026-03-02 17:06:47 -0300

    FINALMENTE NÂO BUGOU O COMMIT
    Back grand versao 1.0

--- first-game/scripts/player.gd
+++ first-game/scripts/player.gd
@@ -1,8 +1,7 @@
 extends CharacterBody2D
-
-
+@onready var _animated_sprite = $AnimatedSprite2D # Substitua pelo nome real do nó na cena
 const SPEED = 130.0
-const JUMP_VELOCITY = -300.0
+const  JUMP_VELOCITY = -300.0
 
 
 func _physics_process(delta: float) -> void:
@@ -10,16 +9,23 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * SPEED
+		if is_on_floor():
+			_animated_sprite.play("run")
+		_animated_sprite.flip_h = direction < 0 
+	
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		_animated_sprite.play("idle")
+		
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		_animated_sprite.play("roll")
+	
 
 	move_and_slide()
