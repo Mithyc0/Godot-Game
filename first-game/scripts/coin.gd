b88709f0732bb501205ccb8323c530edf819818d
Kauê <lukeloke007@gmail.com>
2026-03-11 08:04:43 -0300

    Coloquei o contador de moedas

--- first-game/scripts/coin.gd
+++ first-game/scripts/coin.gd
@@ -4,5 +4,14 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	print("hi")
+	
+	var label_pontos = %MoedasPegas
+	var valor_atual = int(label_pontos.text)
+	var novo_valor = valor_atual+1
+	label_pontos.text = str(novo_valor)
+	#var label_pontos = get_node("/root/Game/CanvasLayer/MoedasPegas")
+	#print(label_pontos.global_position) 
+	
+
 	queue_free()
 	pass # Replace with function body.
