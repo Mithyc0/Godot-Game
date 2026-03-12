bccce7a93d39e9c803a454946e3451ac29118068
Ismael Paez do Carmo <mithyc.paez@gmail.com>
2026-03-12 14:03:26 -0300

    slime
    inimigo e player com pulo bolado (insano)

--- first-game/scripts/player.gd
+++ first-game/scripts/player.gd
@@ -1,31 +1,36 @@
 extends CharacterBody2D
-@onready var _animated_sprite = $AnimatedSprite2D # Substitua pelo nome real do nó na cena
+
+var life = true
+
 const SPEED = 130.0
 const  JUMP_VELOCITY = -300.0
-
+@onready var _animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("move_left", "move_right")
-	if direction:
-		velocity.x = direction * SPEED
-		if is_on_floor():
-			_animated_sprite.play("run")
-		_animated_sprite.flip_h = direction < 0 
-	
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		_animated_sprite.play("idle")
+	if life:
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var direction := Input.get_axis("move_left", "move_right")
 		
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		_animated_sprite.play("roll")
-	
-
-	move_and_slide()
+		# Add the gravity.
+		if not is_on_floor():
+			velocity += get_gravity() * delta
+			_animated_sprite.play("roll")
+			
+		if direction:
+			velocity.x = direction * SPEED
+			
+			if is_on_floor():
+				_animated_sprite.play("run")
+			_animated_sprite.flip_h = direction < 0 
+		
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			_animated_sprite.play("idle")
+			
+		# Handle jump.
+		if Input.is_action_just_pressed("jump"):
+			velocity.y = JUMP_VELOCITY
+		move_and_slide()
+	else:
+		_animated_sprite.play("death")
