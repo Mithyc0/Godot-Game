2e6054791b022ed7dcd52fa4ae2b6b5d11e765cf
Ismael Paez do Carmo <mithyc.paez@gmail.com>
2026-03-17 17:13:17 -0300

    Script do Player, Mapa e Labels
    o script do player ta mais organizado e adicionei texto no mapa do jogo

--- first-game/scripts/player.gd
+++ first-game/scripts/player.gd
@@ -7,30 +7,29 @@ const  JUMP_VELOCITY = -300.0
 @onready var _animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
-	if life:
-		# Get the input direction and handle the movement/deceleration.
-		# As good practice, you should replace UI actions with custom gameplay actions.
-		var direction := Input.get_axis("move_left", "move_right")
-		
-		# Add the gravity.
-		if not is_on_floor():
-			velocity += get_gravity() * delta
-			_animated_sprite.play("roll")
-			
-		if direction:
-			velocity.x = direction * SPEED
-			
-			if is_on_floor():
-				_animated_sprite.play("run")
-			_animated_sprite.flip_h = direction < 0 
-		
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
-			_animated_sprite.play("idle")
-			
-		# Handle jump.
-		if Input.is_action_just_pressed("jump"):
-			velocity.y = JUMP_VELOCITY
-		move_and_slide()
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("move_left", "move_right")
+	
+	# se não está no chão
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		_animated_sprite.play("roll")
+	# se há movimentação e está no chão
+	elif direction:
+		_animated_sprite.play("run")
+	# se não há movimentação e está no chão
 	else:
-		_animated_sprite.play("death")
+		_animated_sprite.play("idle")
+	
+	# Handle move.
+	if direction:
+		velocity.x = direction * SPEED
+		_animated_sprite.flip_h = direction < 0 
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED) 
+		
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	move_and_slide()
