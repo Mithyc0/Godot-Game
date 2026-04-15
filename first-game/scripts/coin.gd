3ad55808d23a4adc9034fefce00d697ebd03cb48
Ismael Paez do Carmo <mithyc.paez@gmail.com>
2026-04-15 13:58:57 -0300

    sem "hi", Score agr

--- first-game/scripts/coin.gd
+++ first-game/scripts/coin.gd
@@ -1,8 +1,10 @@
 extends Area2D
 
-
+# @onready var game_manager: Node = $"../../GameManager"
+# Now its a unique name
+@onready var game_manager: Node = %GameManager
 
 func _on_body_entered(body: Node2D) -> void:
-	print("hi")
+	
 	queue_free()
 	pass # Replace with function body.
